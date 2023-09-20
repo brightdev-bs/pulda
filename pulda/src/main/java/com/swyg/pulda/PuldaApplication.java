3454eede6088fc3ddd1fa778ea619c7d693ad611
Byeongsu KIM <brightdev.bs@gmail.com>
2023-09-20 23:39:21 +0900

    project init

---
+++ pulda/src/main/java/com/swyg/pulda/PuldaApplication.java
@@ -0,0 +1,13 @@
+package com.swyg.pulda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuldaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuldaApplication.class, args);
+    }
+
+}
---
+++ pulda/src/test/java/com/swyg/pulda/PuldaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyg.pulda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuldaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
